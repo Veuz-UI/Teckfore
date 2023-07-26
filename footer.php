9a077aea1bd13f2b5b065ded6dcabad20f80de35
Arshuveuz <arshad@veuz.in>
2023-07-26 20:30:23 +0530

    home page

---
+++ footer.php
@@ -0,0 +1,84 @@
+<section class="footer footer-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="footer-contact">
+                    <img src="images/logo-footer.png" alt="" class="img-fluid logo-white">
+                    <h4 class="footer-sub-heading">PS Arcade, Sarjapur - Marathahalli Rd, Dommasandra, Anekal, Karnataka 562125
+                    </h4>
+                    <ul class="social-media-icons">
+                        <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+                        <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa-brands fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+
+            </div>
+            <div class="col-lg-3">
+                <div class="footer-links">
+
+                    <h5>Quik links</h5>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">How it Works</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Gallery</a></li>
+                        <li><a href="#">Blogs</a></li>
+                        <li><a href="#">Locations</a></li>
+                        <li><a href="#">Careers</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <div class="footer-links">
+
+                    <h5>Location</h5>
+                    <ul>
+                        <li><a href="#">Banglore</a></li>
+                        <li><a href="#">Hyderabad</a></li>
+                        <li><a href="#">Chennai</a></li>
+                        <li><a href="#">Kerala</a></li>
+                        <li><a href="#">Mumbai</a></li>
+                        <li><a href="#">New Delhi</a></li>
+                        <li><a href="#">Visakapatanam</a></li>
+                    </ul>
+
+                </div>
+            </div>
+
+
+            <div class="col-lg-4">
+                <div class="footer-links">
+
+                    <h5>Newsletter</h5>
+                    <ul>
+                        <li><a href="#">Get notified of new products, limited Releases and more</a></li>
+                    </ul>
+                    <div class="row gx-2">
+                        <div class="col-lg-9">
+                            <input type="text" class="footer-inp" placeholder="Email Address">
+                        </div>
+                        <div class="col-lg-3">
+                            <button type="button" class="footer-btn">Subscribe</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <hr class="footer-line2">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 ">
+                <p class="copy-text">&COPY;2023 Teckfore. all rights reserved</p>
+            </div>
+
+        </div>
+    </div>
+</section>
---
+++ header.php
@@ -0,0 +1,49 @@
+<nav class="navbar navbar-expand-lg navbar-light fixed-top navbar-spacing">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="images/logo.png" class="img-fluid main-logo">
+                <img src="images/logo-footer.png" class="img-fluid mobile-main-logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">How it Works</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Blogs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Locations</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Career</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact us</a>
+                    </li>
+
+
+                </ul>
+                <form class="d-flex">
+                    <button class="serch-box-round">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                </form>
+            </div>
+        </div>
+    </nav>
